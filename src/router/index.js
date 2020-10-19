2fb83ef767382701d749aca940a7b0c22f2c45c2
Suhail Ea <suhailedayadi@gmail.com>
2020-10-19 19:35:24 +0530

    updated to history

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Sidebar from '../components/Sidebar.vue'
 vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes:[
     {path:'/sidebar' ,name:'sidebar', component:Sidebar}
   ]
